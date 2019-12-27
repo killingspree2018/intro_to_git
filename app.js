2b75f3ac513b2bab93d3e7c886912a305561d685
Divyansh Jain <jaindivyansh2010@gmail.com>
2019-12-28 00:58:38 +0530

    last commit

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.get("/", function(req,res){
 app.get("/speak", function(req,res){
 	res.render("speak");
 });
-app.listen( 3000, process.env.IP, function(){
+app.listen( process.env.PORT, process.env.IP, function(){
 	console.log("Server has Started");
 });
